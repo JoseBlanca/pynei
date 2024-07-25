fff834649d9edb5b666fceaefe2c36c918ad5e77
Jose Blanca <jblanca@upv.es>
2024-07-25 16:12:20 +0200

    fix in API

--- new_api_proposal.py
+++ new_api_proposal.py
@@ -1,4 +1,3 @@
-
 from typing import Iterator
 
 import pandas
@@ -6,39 +5,39 @@ import numpy
 
 DEF_NUM_VARIANTS = 10000
 
-class VariantsChunk():
-	def __init__(self, variants_info:pandas.DataFrame, alleles:pandas.DataFrame, gts:numpy.array):
-		...
-	
-	@property
-	def num_variants(self):
-		...
-		
-	@property
-	def samples(self):
-	# or indi_names
-		...
 
-	@property		
-	def ploidy(self):
-		...
+class VariantsChunk:
+    def __init__(
+        self,
+        variants_info: pandas.DataFrame,
+        alleles: pandas.DataFrame,
+        gts: numpy.array,
+    ): ...
 
-	@property		
-	def gt_array(self):
-		...
+    @property
+    def num_variants(self): ...
+
+    @property
+    def samples(self):
+        # or indi_names
+        ...
+
+    @property
+    def ploidy(self): ...
+
+    @property
+    def gt_array(self): ...
 
 
 class Variants:
-	def init__(self, variant_chunks:Iterator[VariantsChunk])__:
-		...
-		
-	def get_variant_chunks(self, num_variants_per_chunk=DEF_NUM_VARIANTS, keep_only_gts=False):
-		...
-		
-	@property
-	def num_variants(self):
-		...
-		
-	@property
-	def samples(self):
-		...
+    def __init__(self, variant_chunks: Iterator[VariantsChunk]): ...
+
+    def get_variants_chunks(
+        self, num_variants_per_chunk=DEF_NUM_VARIANTS, keep_only_gts=False
+    ) -> Iterator[VariantsChunk]: ...
+
+    @property
+    def num_variants(self): ...
+
+    @property
+    def samples(self): ...
