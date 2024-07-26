f22bd8bc154ce457255f02cc167e0f959e45fbd4
Jose Blanca <jblanca@upv.es>
2024-07-26 08:23:06 +0200

    variants in memory and not in memory

--- new_api_proposal.py
+++ new_api_proposal.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Iterator, Self
 
 import pandas
 import numpy
@@ -30,14 +30,29 @@ class VariantsChunk:
 
 
 class Variants:
-    def __init__(self, variant_chunks: Iterator[VariantsChunk]): ...
+    def __init__(
+        self, variants_chunks: Iterator[VariantsChunk], store_chunks_in_memory=False
+    ):
+        if store_chunks_in_memory:
+            self._variants_chunks = list(variants_chunks)
+            self._chunks_iter = None
+        else:
+            self._variants_chunks = None
+            self._chunks_iter = variants_chunks
 
     def get_variants_chunks(
         self, num_variants_per_chunk=DEF_NUM_VARIANTS, keep_only_gts=False
-    ) -> Iterator[VariantsChunk]: ...
+    ) -> Iterator[VariantsChunk]:
+        if self._variants_chunks is not None:
+            return iter(self._variants_chunks)
+        else:
+            return self._chunks_iter
 
     @property
     def num_variants(self): ...
 
     @property
     def samples(self): ...
+
+    @classmethod
+    def from_gt_array(cls, gt_array: numpy.array) -> Self: ...
