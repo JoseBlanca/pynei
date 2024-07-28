88ab7029b0f021643fb7323a81a599d33a459021
Jose Blanca <jblanca@upv.es>
2024-07-28 11:39:26 +0200

    implement Genotypes class

--- src/pynei/variants.py
+++ src/pynei/variants.py
@@ -1,5 +1,7 @@
-from typing import Iterator, Self
+from typing import Iterator, Self, Sequence
+from collections.abc import Sequence as SequenceABC
 import itertools
+
 
 import numpy
 import pandas
@@ -12,8 +14,83 @@ from .config import (
 )
 
 
-def is_read_only(arr: numpy.array) -> bool:
-    return not arr.flags.writeable
+class Genotypes:
+    def __init__(
+        self,
+        gt_array: numpy.ndarray,
+        samples: numpy.ndarray | Sequence[str] | None = None,
+    ):
+        if not isinstance(gt_array, numpy.ndarray):
+            raise ValueError("gts must be a numpy array")
+        if not numpy.issubdtype(gt_array.dtype, numpy.integer):
+            raise ValueError("gts must be an integer numpy array")
+
+        assert gt_array.ndim == 3
+
+        if gt_array.flags.writeable:
+            gt_array = gt_array.copy()
+            gt_array.flags.writeable = False
+
+        if samples is not None:
+            samples = numpy.array(samples)
+            samples.flags.writeable = False
+            if len(set(samples)) < samples.size:
+                unique_elements, counts = numpy.unique(samples, return_counts=True)
+                duplicated_samples = unique_elements[counts > 1]
+                raise ValueError(f"Duplicated sample names: {duplicated_samples}")
+
+        else:
+            samples = numpy.arange(gt_array.shape[1])
+        samples.flags.writeable = False
+
+        if gt_array.shape[1] != samples.size:
+            raise ValueError(
+                f"Number of samples in gts ({gt_array.shape[1]}) and number of given samples ({samples.size}) do not match"
+            )
+
+        self._gts = gt_array
+        self._samples = samples
+
+    @property
+    def samples(self):
+        return self._samples
+
+    @property
+    def gt_array(self):
+        return self._gts
+
+    @property
+    def shape(self):
+        return self._gts.shape
+
+    @property
+    def num_vars(self):
+        return self._gts.shape[0]
+
+    @property
+    def num_samples(self):
+        return self._gts.shape[1]
+
+    @property
+    def ploidy(self):
+        return self._gts.shape[2]
+
+    def get_vars(self, index):
+        gts = self.gt_array[index, :, :]
+
+        gts.flags.writeable = False
+        return self.__class__(gt_array=gts, samples=self.samples)
+
+    def get_samples(self, samples: Sequence[str] | Sequence[int]) -> Self:
+        if not isinstance(samples, SequenceABC):
+            raise ValueError("samples must be a sequence")
+        index = numpy.where(numpy.isin(self.samples, samples))[0]
+        gts = self.gt_array[:, index, :]
+        samples = self.samples[index]
+
+        samples.flags.writeable = False
+        gts.flags.writeable = False
+        return self.__class__(gt_array=gts, samples=samples)
 
 
 def _normalize_pandas_types(dframe):
@@ -37,59 +114,43 @@ def _normalize_pandas_types(dframe):
 class VariantsChunk:
     def __init__(
         self,
-        gts: numpy.array,
-        variants_info: pandas.DataFrame | None = None,
+        gts: Genotypes,
+        vars_info: pandas.DataFrame | None = None,
         alleles: pandas.DataFrame | None = None,
-        samples: list[str] | None = None,
     ):
-        if not isinstance(gts, numpy.ndarray):
-            raise ValueError("gts must be a numpy array")
-        if not numpy.issubdtype(gts.dtype, numpy.integer):
-            raise ValueError("gts must be an integer numpy array")
-        if gts.ndim != 3:
-            raise ValueError(
-                "gts must be a 3D numpy array: (num_vars, num_samples, ploidy)"
-            )
-
-        if not is_read_only(gts):
-            gts = gts.copy()
-            gts.flags.writeable = False
-
-        if variants_info is not None:
-            if variants_info.shape[0] != gts.shape[0]:
+        if not isinstance(gts, Genotypes):
+            raise ValueError("gts must be a Genotypes object")
+        if vars_info is not None:
+            if vars_info.shape[0] != gts.num_vars:
                 raise ValueError(
                     "variants_info must have the same number of rows as gts"
                 )
-            variants_info = _normalize_pandas_types(variants_info)
+            vars_info = _normalize_pandas_types(vars_info)
 
         if alleles is not None:
-            if alleles.shape[0] != gts.shape[0]:
+            if alleles.shape[0] != gts.num_vars:
                 raise ValueError("alleles must have the same number of rows as gts")
 
-        if samples is not None:
-            if len(samples) != gts.shape[1]:
-                raise ValueError("There has to be as many samples gts.shape[1]")
-
+        self._arrays = {"gts": gts, "vars_info": vars_info, "alleles": alleles}
         self._gt_array = gts
-        self._vars_info = variants_info
+        self._vars_info = vars_info
         self._alleles = alleles
-        self.samples = samples
 
     @property
     def num_vars(self):
-        return self._gt_array.shape[0]
+        return self._arrays["gts"].num_vars
 
     @property
     def num_samples(self):
-        return self._gt_array.shape[1]
+        return self._arrays["gts"].num_samples
 
     @property
     def ploidy(self):
-        return self._gt_array.shape[2]
+        return self._arrays["gts"].ploidy
 
     @property
     def gts(self):
-        return self._gt_array
+        return self._arrays["gts"]
 
 
 class Variants:
@@ -97,6 +158,7 @@ class Variants:
         self, vars_chunks: Iterator[VariantsChunk], store_chunks_in_memory=False
     ):
         vars_chunks = iter(vars_chunks)
+        self._first_chunk = None
         if store_chunks_in_memory:
             self._vars_chunks = list(vars_chunks)
             self._chunks_iter = None
@@ -111,6 +173,8 @@ class Variants:
             return self._chunks_iter
 
     def _get_first_chunk(self):
+        if self._first_chunk is not None:
+            return self._first_chunk
         if self._vars_chunks is not None:
             chunk = self._vars_chunks[0]
         else:
@@ -119,16 +183,17 @@ class Variants:
             except StopIteration:
                 raise RuntimeError("No variants_chunks available")
             self._chunks_iter = itertools.chain([chunk], self._chunks_iter)
+            self._first_chunk = chunk
         return chunk
 
     def iter_vars_chunks(
-        self, num_vars_per_chunk=DEF_NUM_VARS_PER_CHUNK
+        self, desired_num_vars_per_chunk=DEF_NUM_VARS_PER_CHUNK
     ) -> Iterator[VariantsChunk]:
         return self._get_orig_vars_iter()
 
     @property
     def samples(self):
-        return self._get_first_chunk().samples
+        return self._get_first_chunk().gts.samples
 
     @classmethod
     def from_gt_array(
@@ -136,5 +201,5 @@ class Variants:
         gts: numpy.array,
         samples: list[str] | None = None,
     ) -> Self:
-        chunk = VariantsChunk(gts=gts, samples=samples)
+        chunk = VariantsChunk(gts=Genotypes(gts, samples=samples))
         return cls(vars_chunks=[chunk], store_chunks_in_memory=True)
--- test/test_vars.py
+++ test/test_vars.py
@@ -2,8 +2,35 @@ import numpy
 import pandas
 import pytest
 
-from pynei.variants import VariantsChunk, Variants
+from pynei.variants import Genotypes, VariantsChunk, Variants
 from pynei.config import CHROM_VARIANTS_COL, POS_VARIANTS_COL
+
+
+def test_gts():
+    num_vars = 3
+    num_samples = 4
+    ploidy = 2
+    gt_array = numpy.random.randint(0, 2, size=(num_vars, num_samples, ploidy))
+    gts = Genotypes(gt_array)
+    assert gts.num_vars == num_vars
+    assert gts.num_samples == num_samples
+    assert gts.ploidy == ploidy
+    assert numpy.array_equal(gts.samples, numpy.arange(num_samples))
+    assert numpy.array_equal(gts.gt_array, gt_array)
+
+    gts = Genotypes(gt_array, samples=["a", "b", "c", "d"])
+    vars_slice = [1, 2]
+    gts2 = gts.get_vars(vars_slice)
+    assert numpy.array_equal(gts2.gt_array, gt_array[vars_slice, :, :])
+
+    gts2 = gts.get_samples(["b", "d"])
+    assert numpy.array_equal(gts2.gt_array, gt_array[:, [1, 3], :])
+
+    with pytest.raises(ValueError):
+        gts = Genotypes(gt_array, samples=["a", "a", "c", "d"])
+
+    with pytest.raises(ValueError):
+        gts = Genotypes(gt_array, samples=["a", "b"])
 
 
 def test_chunk():
@@ -11,7 +38,7 @@ def test_chunk():
     num_samples = 4
     ploidy = 2
     gt_array = numpy.random.randint(0, 2, size=(num_vars, num_samples, ploidy))
-    chunk = VariantsChunk(gts=gt_array)
+    chunk = VariantsChunk(gts=Genotypes(gt_array))
     assert chunk.num_vars == num_vars
     assert chunk.num_samples == num_samples
     assert chunk.ploidy == ploidy
@@ -22,30 +49,27 @@ def test_chunk_different_num_rows():
     num_samples = 4
     ploidy = 2
     gt_array = numpy.random.randint(0, 2, size=(num_vars, num_samples, ploidy))
-    variants_info = pandas.DataFrame(
+    gts = Genotypes(gt_array)
+    vars_info = pandas.DataFrame(
         {
             CHROM_VARIANTS_COL: ["chr1", "chr1", "chr2"],
             POS_VARIANTS_COL: [1, 2, 3],
         }
     )
-    chunk = VariantsChunk(gts=gt_array, variants_info=variants_info)
+    chunk = VariantsChunk(gts=gts, vars_info=vars_info)
     assert chunk.num_vars == num_vars
 
     variants_info2 = pandas.DataFrame(
         {CHROM_VARIANTS_COL: ["chr1", "chr1"], POS_VARIANTS_COL: [1, 2]}
     )
     with pytest.raises(ValueError):
-        VariantsChunk(gts=gt_array, variants_info=variants_info2)
+        VariantsChunk(gts=gts, vars_info=variants_info2)
 
     alleles = pandas.DataFrame([["A", "T", ""], ["C", "G", ""], ["A", "C", "T"]])
-    VariantsChunk(gts=gt_array, alleles=alleles)
+    VariantsChunk(gts=gts, alleles=alleles)
 
     with pytest.raises(ValueError):
-        VariantsChunk(gts=gt_array, alleles=alleles.iloc[:1, :])
-
-    VariantsChunk(gts=gt_array, samples=["a", "b", "c", "d"])
-    with pytest.raises(ValueError):
-        VariantsChunk(gts=gt_array, samples=["a", "b"])
+        VariantsChunk(gts=gts, alleles=alleles.iloc[:1, :])
 
 
 def test_variants_from_gts():
@@ -55,13 +79,14 @@ def test_variants_from_gts():
     ploidy = 2
     gt_array = numpy.random.randint(0, 2, size=(num_vars, num_samples, ploidy))
     variants = Variants.from_gt_array(gt_array, samples=samples)
-    assert variants.samples == samples
-    assert numpy.array_equal(next(variants.iter_vars_chunks()).gts, gt_array)
+    assert all(numpy.equal(variants.samples, samples))
+    assert numpy.array_equal(next(variants.iter_vars_chunks()).gts.gt_array, gt_array)
 
-    chunk = VariantsChunk(gts=gt_array, samples=samples)
+    gts = Genotypes(gt_array, samples=samples)
+    chunk = VariantsChunk(gts=gts)
     variants = Variants(vars_chunks=[chunk])
-    assert variants.samples == samples
-    assert numpy.array_equal(next(variants.iter_vars_chunks()).gts, gt_array)
+    assert all(numpy.equal(variants.samples, samples))
+    assert numpy.array_equal(next(variants.iter_vars_chunks()).gts.gt_array, gt_array)
 
 
 def test_chunk_size():
@@ -69,16 +94,16 @@ def test_chunk_size():
     num_samples = 3
     ploidy = 2
     gt_array = numpy.random.randint(0, 2, size=(num_vars, num_samples, ploidy))
-    chunk = VariantsChunk(gts=gt_array)
+    chunk = VariantsChunk(gts=Genotypes(gt_array))
     variants = Variants(vars_chunks=[chunk], store_chunks_in_memory=True)
-    chunks = list(variants.iter_vars_chunks(num_vars_per_chunk=10))
+    chunks = list(variants.iter_vars_chunks(desired_num_vars_per_chunk=10))
     print(chunks[0].num_vars)
     print(len(chunks))
 
-    chunks = list(variants.iter_vars_chunks(num_vars_per_chunk=100))
+    chunks = list(variants.iter_vars_chunks(desired_num_vars_per_chunk=100))
     print(chunks[0].num_vars)
     print(len(chunks))
 
-    chunks = list(variants.iter_vars_chunks(num_vars_per_chunk=200))
+    chunks = list(variants.iter_vars_chunks(desired_num_vars_per_chunk=200))
     print(chunks[0].num_vars)
     print(len(chunks))
