0b769c9623d119e589603bde7ae57045e560f509
Jose Blanca <jblanca@upv.es>
2024-07-29 14:41:55 +0200

    remove old name

--- src/pynei/config.py
+++ src/pynei/config.py
@@ -3,7 +3,6 @@ from enum import Enum
 import pandas
 
 MISSING_ALLELE = -1
-DEFAULT_NAME_POP_ALL_INDIS = "all_indis"
 MIN_NUM_GENOTYPES_FOR_POP_STAT = 20
 DEF_POLY_THRESHOLD = 0.95
 CHROM_VARIANTS_COL = "chrom"
