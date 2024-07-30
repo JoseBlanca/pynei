37d7558dd5b7d42d3ff0a9b2b6b26af1e299f6c0
Jose Blanca <jblanca@upv.es>
2024-07-30 16:08:15 +0200

    implement to mat012 for PCA

--- src/pynei/pca.py
+++ src/pynei/pca.py
@@ -1,151 +1,42 @@
+from functools import partial
 import numpy
-import pandas
 
-from .genotypes import Genotypes
-from .dists import Distances, calc_kosman_pairwise_dists
-
-
-def _create_pc_names(num_prin_comps):
-    n_digits = num_prin_comps // 10
-    fstring = "{:0" + str(n_digits) + "d}"
-    prin_comps_names = ["PC" + fstring.format(idx) for idx in range(num_prin_comps)]
-    return prin_comps_names
+from pynei.gt_counts import _count_alleles_per_var
+from pynei.config import DEF_POP_NAME, MISSING_ALLELE
+from pynei.pipeline import Pipeline
 
 
-def do_pca(data: pandas.DataFrame, center_data=True, standarize_data=True):
-    if numpy.any(numpy.isnan(data)):
-        raise ValueError("data can have no nan values")
+def _create_012_gt_matrix(chunk, transform_to_biallelic=False):
+    res = _count_alleles_per_var(chunk, calc_freqs=False)
+    allele_counts = res["counts"][DEF_POP_NAME]["allele_counts"].values
 
-    if standarize_data and not center_data:
-        raise ValueError("If you standarize you have to also center the data")
+    num_genotyped_alleles_per_var = allele_counts.sum(axis=1)
+    if numpy.any(num_genotyped_alleles_per_var == 0):
+        raise ValueError("There are variants that only have missing data")
 
-    trait_names = data.columns
-    sample_names = data.index
+    max_num_alleles = allele_counts.shape[1]
+    if max_num_alleles > 2 and not transform_to_biallelic:
+        raise ValueError(
+            f"In order to get the 012 matrix you should pass transform_to_biallelic=True, because you have {max_num_alleles} in at least one variation"
+        )
 
-    data = data.values
-    num_samples, num_traits = data.shape
-
-    if center_data:
-        data = data - data.mean(axis=0)
-
-    if standarize_data:
-        data = data / data.std(axis=0)
-
-    U, Sigma, Vh = numpy.linalg.svd(data, full_matrices=False)
-    singular_vals = Sigma
-    prin_comps = Vh
-    num_prin_comps = prin_comps.shape[0]
-    prin_comps_names = _create_pc_names(num_prin_comps)
-
-    eig_vals = numpy.square(singular_vals) / (num_samples - 1)
-    pcnts = eig_vals / eig_vals.sum() * 100.0
-    projections = numpy.dot(prin_comps, data.T).T
-
-    return {
-        "projections": pandas.DataFrame(
-            projections, index=sample_names, columns=prin_comps_names
-        ),
-        "explained_variance (%)": pandas.Series(pcnts, index=prin_comps_names),
-        "princomps": pandas.DataFrame(
-            prin_comps, index=prin_comps_names, columns=trait_names
-        ),
-    }
+    major_alleles = numpy.argmax(allele_counts, axis=1)
+    gt_array = chunk.gts.gt_array
+    gts012 = numpy.sum(gt_array != major_alleles[:, None, None], axis=2)
+    gts012[numpy.any(gt_array == MISSING_ALLELE, axis=2)] = MISSING_ALLELE
+    return gts012
 
 
-def do_pca_with_genotypes(gts: Genotypes, transform_to_biallelic=False):
-    mat012 = gts.get_mat_012(transform_to_biallelic=transform_to_biallelic)
-    mat012 = pandas.DataFrame(mat012.T, index=gts.indi_names)
-    return do_pca(mat012, center_data=True, standarize_data=True)
+def _append_array(array: numpy.ndarray | None, array_to_append: numpy.ndarray):
+    if array is None:
+        return array_to_append
+
+    return numpy.vstack((array, array_to_append))
 
 
-def _make_f_matrix(matrix):
-    """It takes an E matrix and returns an F matrix
-
-    The input is the output of make_E_matrix
-
-    For each element in matrix subtract mean of corresponding row and
-    column and add the mean of all elements in the matrix
-    """
-    num_rows, num_cols = matrix.shape
-    # make a vector of the means for each row and column
-    # column_means = (numpy.add.reduce(E_matrix) / num_rows)
-    column_means = (numpy.add.reduce(matrix) / num_rows)[:, numpy.newaxis]
-    trans_matrix = numpy.transpose(matrix)
-    row_sums = numpy.add.reduce(trans_matrix)
-    row_means = row_sums / num_cols
-    # calculate the mean of the whole matrix
-    matrix_mean = numpy.sum(row_sums) / (num_rows * num_cols)
-    # adjust each element in the E matrix to make the F matrix
-
-    matrix -= row_means
-    matrix -= column_means
-    matrix += matrix_mean
-
-    return matrix
-
-
-def do_pcoa(dists: Distances):
-    "It does a Principal Coordinate Analysis on a distance matrix"
-    # the code for this function is taken from pycogent metric_scaling.py
-    # Principles of Multivariate analysis: A User's Perspective.
-    # W.J. Krzanowski Oxford University Press, 2000. p106.
-
-    sample_names = dists.names
-    dists = dists.square_dists.values
-
-    if numpy.any(numpy.isnan(dists)):
-        raise ValueError("dists array has nan values")
-
-    e_matrix = (dists * dists) / -2.0
-    f_matrix = _make_f_matrix(e_matrix)
-
-    eigvals, eigvecs = numpy.linalg.eigh(f_matrix)
-    eigvecs = eigvecs.transpose()
-    # drop imaginary component, if we got one
-    eigvals, eigvecs = eigvals.real, eigvecs.real
-
-    # convert eigvals and eigvecs to point matrix
-    # normalized eigenvectors with eigenvalues
-
-    # get the coordinates of the n points on the jth axis of the Euclidean
-    # representation as the elements of (sqrt(eigvalj))eigvecj
-    # must take the absolute value of the eigvals since they can be negative
-    pca_matrix = eigvecs * numpy.sqrt(abs(eigvals))[:, numpy.newaxis]
-
-    # output
-    # get order to output eigenvectors values. reports the eigvecs according
-    # to their cooresponding eigvals from greatest to least
-    vector_order = list(numpy.argsort(eigvals))
-    vector_order.reverse()
-
-    eigvals = eigvals[vector_order]
-
-    # eigenvalues
-    pcnts = (eigvals / numpy.sum(eigvals)) * 100.0
-
-    # the outputs
-    # eigenvectors in the original pycogent implementation, here we name them
-    # princoords
-    # I think that we're doing: if the eigenvectors are written as columns,
-    # the rows of the resulting table are the coordinates of the objects in
-    # PCO space
-    projections = []
-    for name_i in range(dists.shape[0]):
-        eigvect = [pca_matrix[vec_i, name_i] for vec_i in vector_order]
-        projections.append(eigvect)
-    projections = numpy.array(projections)
-    prin_comps_names = _create_pc_names(projections.shape[1])
-
-    return {
-        "projections": pandas.DataFrame(
-            projections, index=sample_names, columns=prin_comps_names
-        ),
-        "explained_variance (%)": pandas.Series(pcnts, index=prin_comps_names),
-    }
-
-
-def do_pcoa_with_genotypes(gts: Genotypes, use_approx_embedding_algorithm=False):
-    dists = calc_kosman_pairwise_dists(
-        gts, use_approx_embedding_algorithm=use_approx_embedding_algorithm
+def create_012_gt_matrix(variants, transform_to_biallelic=False):
+    create_012_matrix = partial(
+        _create_012_gt_matrix, transform_to_biallelic=transform_to_biallelic
     )
-    return do_pcoa(dists)
+    pipeline = Pipeline(map_functs=[create_012_matrix], reduce_funct=_append_array)
+    return pipeline.map_and_reduce(variants)
--- test/test_pca.py
+++ test/test_pca.py
@@ -1,51 +1,45 @@
+import pytest
 import numpy
 
-from datasets import IRIS
-
-from pynei import (
-    do_pca,
-    Genotypes,
-    do_pca_with_genotypes,
-    do_pcoa,
-    do_pcoa_with_genotypes,
-)
-from pynei.dists import Distances
+from pynei.pca import _create_012_gt_matrix, create_012_gt_matrix
+from pynei.variants import Variants
+from pynei.config import MISSING_ALLELE
 
 
-def test_pca():
-    pca = do_pca(IRIS["characterization"])
-
-    expected_princomps = [
-        [0.52106591, -0.26934744, 0.5804131, 0.56485654],
-        [-0.37741762, -0.92329566, -0.02449161, -0.06694199],
-        [0.71956635, -0.24438178, -0.14212637, -0.63427274],
-        [0.26128628, -0.12350962, -0.80144925, 0.52359713],
-    ]
-    princomps = pca["princomps"].values
-    for idx in range(princomps.shape[0]):
-        assert numpy.allclose(
-            expected_princomps[idx], princomps[idx]
-        ) or numpy.allclose(expected_princomps[idx], -princomps[idx])
-
-
-def test_pca_gt():
-    numpy.random.seed(seed=42)
-    num_vars = 100
-    num_indis = 20
+def test_mat012():
+    num_vars = 3
+    num_indis = 4
     ploidy = 2
-    gt_array = numpy.random.randint(0, 2, size=(num_vars, num_indis, ploidy))
-    gts = Genotypes(gt_array)
-    do_pca_with_genotypes(gts)
+    gt_array = numpy.random.randint(0, 1, size=(num_vars, num_indis, ploidy))
+    gt_array[0, 0, 0] = 0
+    gt_array[0, 0, 1] = 1
+    gt_array[1, :, :] = 2
+    gt_array[1, 0, 1] = 3
+    gt_array[2, :, :] = MISSING_ALLELE
 
-    do_pcoa_with_genotypes(gts, use_approx_embedding_algorithm=False)
-    do_pcoa_with_genotypes(gts, use_approx_embedding_algorithm=True)
+    vars = Variants.from_gt_array(gt_array)
+    chunk = next(vars.iter_vars_chunks())
+    with pytest.raises(ValueError):
+        _create_012_gt_matrix(chunk)
 
+    gt_array = numpy.random.randint(0, 1, size=(num_vars, num_indis, ploidy))
+    gt_array[0, 0, 0] = 0
+    gt_array[0, 0, 1] = 1
+    gt_array[0, 1, 0] = 1
+    gt_array[0, 1, 1] = 1
+    gt_array[1, :, :] = 2
+    gt_array[1, 0, 1] = 3
+    gt_array[1, 3, 0] = 3
+    gt_array[1, 3, 1] = 3
+    vars = Variants.from_gt_array(gt_array)
+    chunk = next(vars.iter_vars_chunks())
+    with pytest.raises(ValueError):
+        _create_012_gt_matrix(chunk)
 
-def test_pcoa():
-    dists = [0.2, 0.3, 0.9, 0.9, 0.1, 0.8, 0.7, 0.7, 0.8, 0.2]
-    dists = Distances(numpy.array(dists), names=["i1", "i2", "i3", "i4", "i5"])
-    res = do_pcoa(dists)
-    projections = res["projections"]
-    assert abs(projections.loc["i1", "PC0"] - projections.loc["i2", "PC0"]) < abs(
-        projections.loc["i1", "PC0"] - projections.loc["i4", "PC0"]
-    )
+    mat012 = _create_012_gt_matrix(chunk, transform_to_biallelic=True)
+    expected = [[1, 2, 0, 0], [1, 0, 0, 2], [0, 0, 0, 0]]
+    assert numpy.all(mat012 == expected)
+
+    vars.desired_num_vars_per_chunk = 2
+    mat012_2 = create_012_gt_matrix(vars, transform_to_biallelic=True)
+    assert numpy.array_equal(mat012, mat012_2)
