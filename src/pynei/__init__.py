4108085c6f30a77b507326a6be27a6abf0316723
Jose Blanca <jblanca@upv.es>
2024-07-30 08:25:04 +0200

    old public API removed

--- src/pynei/__init__.py
+++
@@ -1,9 +0,0 @@
-from .genotypes import Genotypes
-from .stats import (
-    calc_obs_het,
-    calc_major_allele_freqs,
-    calc_poly_vars_ratio,
-    calc_exp_het,
-    calc_allele_freq_spectrum,
-)
-from .pca import do_pca, do_pca_with_genotypes, do_pcoa, do_pcoa_with_genotypes
