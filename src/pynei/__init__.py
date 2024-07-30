094dedc2ae0f35b1f5eec9084c253401b90584e9
Jose Blanca <jblanca@upv.es>
2024-07-30 10:17:20 +0200

    populate new API

---
+++ src/pynei/__init__.py
@@ -0,0 +1,3 @@
+from pynei.variants import Variants
+from pynei.gt_counts import calc_obs_het_stats_per_var, calc_major_allele_stats_per_var
+from pynei.diversity import calc_exp_het_stats_per_var, calc_poly_vars_ratio_per_var
