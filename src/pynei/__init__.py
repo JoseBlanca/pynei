5fecb164388889b71befe4ce742c869818d561a9
Jose Blanca <jblanca@upv.es>
2024-08-22 11:10:28 +0200

    add vars_from_vcf to API

--- src/pynei/__init__.py
+++ src/pynei/__init__.py
@@ -3,3 +3,4 @@ from pynei.gt_counts import calc_obs_het_stats_per_var, calc_major_allele_stats_
 from pynei.diversity import calc_exp_het_stats_per_var, calc_poly_vars_ratio_per_var
 from pynei.pca import do_pca_with_vars, do_pca, do_pcoa
 from pynei.dists import calc_pairwise_kosman_dists
+from pynei.io_vcf import vars_from_vcf
