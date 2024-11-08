0ad205db69a73335fd429f7e4baed448effcdcfa
Jose Blanca <jblanca@upv.es>
2024-11-08 11:44:38 +0100

    implement filter vars by maf

--- src/pynei/var_filters.py
+++ src/pynei/var_filters.py
@@ -3,7 +3,7 @@ from functools import partial
 import numpy
 
 from pynei.variants import Variants
-from pynei.gt_counts import _calc_gt_is_missing
+from pynei.gt_counts import _calc_gt_is_missing, _calc_maf_per_var
 
 
 class _FilterIterFactory:
@@ -34,7 +34,23 @@ def filter_by_missing_data(vars: Variants, max_missing_rate: float = 0.0) -> Var
     )
 
 
+def _filter_chunk_by_maf(chunk, max_allowed_maf):
+    mafs = _calc_maf_per_var(chunk, pops={"pop": slice(None, None)}, min_num_samples=0)[
+        "major_allele_freqs_per_var"
+    ]["pop"]
+    chunk = chunk.get_vars(mafs <= max_allowed_maf)
+    return chunk
+
+
+def filter_by_maf(vars: Variants, max_allowed_maf) -> Variants:
+    filter_chunk = partial(_filter_chunk_by_maf, max_allowed_maf=max_allowed_maf)
+    chunk_factory = _FilterIterFactory(vars, filter_chunk)
+    return Variants(
+        vars_chunk_iter_factory=chunk_factory,
+        desired_num_vars_per_chunk=vars.desired_num_vars_per_chunk,
+    )
+
+
 # TODO
-# poly_rate
 # obs_het
 # var QUAL
--- test/test_filters.py
+++ test/test_filters.py
@@ -2,7 +2,7 @@ import pytest
 import numpy
 
 from pynei.variants import Variants
-from pynei.var_filters import filter_by_missing_data
+from pynei.var_filters import filter_by_missing_data, filter_by_maf
 
 
 def test_filter_missing():
@@ -25,3 +25,22 @@ def test_filter_missing():
     vars = filter_by_missing_data(orig_vars, max_missing_rate=-0.1)
     with pytest.raises(StopIteration):
         next(vars.iter_vars_chunks())
+
+
+def test_filter_mafs():
+    gts = numpy.array(
+        [
+            [[0, 0], [2, 1], [0, 0], [0, 0], [0, 0]],
+            [[0, 0], [0, 0], [0, 1], [1, 0], [1, 1]],
+            [[1, 1], [1, 1], [1, 1], [1, 1], [1, 1]],
+        ]
+    )
+    orig_vars = Variants.from_gt_array(gts, samples=[0, 1, 2, 3, 4])
+    vars = filter_by_maf(orig_vars, max_allowed_maf=0.9)
+    filtered_gts = numpy.ma.getdata(next(vars.iter_vars_chunks())._gt_array._gts)
+    assert numpy.all(gts[[True, True, False], ...] == filtered_gts)
+
+    orig_vars = Variants.from_gt_array(gts, samples=[0, 1, 2, 3, 4])
+    vars = filter_by_maf(orig_vars, max_allowed_maf=0.7)
+    filtered_gts = numpy.ma.getdata(next(vars.iter_vars_chunks())._gt_array._gts)
+    assert numpy.all(gts[[False, True, False], ...] == filtered_gts)
