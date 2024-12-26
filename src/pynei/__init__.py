9e5a79cdee0e2f31cb5360089f00ce8ab1db1b6c
Jose Blanca <jblanca@upv.es>
2024-12-26 12:42:00 +0100

    pop dists calculation should be public

--- src/pynei/__init__.py
+++ src/pynei/__init__.py
@@ -3,7 +3,7 @@ from pynei.config import VAR_TABLE_POS_COL, VAR_TABLE_CHROM_COL
 from pynei.gt_counts import calc_obs_het_stats_per_var, calc_major_allele_stats_per_var
 from pynei.diversity import calc_exp_het_stats_per_var, calc_poly_vars_ratio_per_var
 from pynei.pca import do_pca_with_vars, do_pca, do_pcoa
-from pynei.dists import calc_pairwise_kosman_dists
+from pynei.dists import calc_pairwise_kosman_dists, calc_jost_dest_pop_dists
 from pynei.io_vcf import vars_from_vcf
 from pynei.var_filters import (
     filter_by_missing_data,
